56c02035c315d993bd9a74c62e61ccd12c05dd76
ReplayCoding <replaycoding@gmail.com>
2022-07-24 11:18:16 -0400

    add license and usage

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "vtableextractor.hpp"
 #include <LIEF/LIEF.hpp>
 #include <fmt/core.h>
-#include <fmt/color.h>
 
 int main(int argc, const char **argv) {
-  if (argc < 2)
+  if (argc != 2) {
+    fmt::print("usage: {} <binary>\n", argv[0]);
     return 1;
+  };
   auto binary = LIEF::Parser::parse(argv[1]);
   auto vtables = VtableExtractor(*binary).get_vtables();
 
@@ -20,7 +21,8 @@ int main(int argc, const char **argv) {
       } else {
         formatted_name = member.fixed_name;
       };
-      fmt::print("\t{} is at offset {} (member# {})\n", formatted_name, offset, offset / vtable.pointer_size);
+      fmt::print("\t{} is at offset {} (member# {})\n", formatted_name, offset,
+                 offset / vtable.pointer_size);
     };
   }
 };
