39f806284c15053d21ce02e202ef96207ffe106c
ReplayCoding <replaycoding@gmail.com>
2022-07-31 12:53:32 -0400

    IT WORKS

--- vtableextractor.hpp
+++ vtableextractor.hpp
@@ -4,7 +4,15 @@
 
 static std::array<std::string, 3> typeinfo_type_lookup = {
     "CLASS_TYPE_INFO", "SI_CLASS_TYPE_INFO", "VMI_CLASS_TYPE_INFO"};
-class StringError;
+
+class StringError : public std::exception {
+public:
+  StringError(std::string s) : s(s){};
+  virtual const char *what() const noexcept override { return s.c_str(); };
+
+private:
+  std::string s;
+};
 class VtableExtractor {
 public:
   VtableExtractor(LIEF::Binary &binary);
@@ -20,13 +28,22 @@ public:
     std::string name;
 
     // __si_class_type_info
-    std::shared_ptr<typeinfo_t> base_type;
+    std::shared_ptr<typeinfo_t> si_base_class;
 
     // __vmi_class_type_info
+    struct vmi_base_class_t {
+      std::shared_ptr<typeinfo_t> base_class;
+      // I'm not completely sure if this is signed or not but thats what the
+      // docs say sooooo
+      int64_t offset_flags;
+    };
+    uint32_t vmi_flags;
+    uint32_t vmi_base_count;
+    std::vector<vmi_base_class_t> vmi_base_class_info{};
   };
   struct vtable_member_t {
     LIEF::Symbol symbol;
-    std::string fixed_name;
+    std::string fixed_name{};
   };
   struct vtable_data_t {
     std::string name{};
@@ -42,23 +59,24 @@ public:
 
 private:
   int get_pointer_size_for_bin();
-  uint64_t get_pointer_data_at_offset(uint64_t virtual_addr);
   std::string fixup_symbol_name(std::string name);
   void generate_symbol_map();
   std::string get_typeinfo_name(uint64_t name);
   typeinfo_t parse_typeinfo(uint64_t addr);
   vtable_data_t get_vtable(uint64_t addr);
 
+  template <typename T> T get_data_at_offset(uint64_t virtual_addr);
+  inline uint64_t get_ptr_at_offset(uint64_t virtual_addr) {
+    if (pointer_size_for_binary == 4) {
+      return get_data_at_offset<uint32_t>(virtual_addr);
+    } else if (pointer_size_for_binary == 8) {
+      return get_data_at_offset<uint64_t>(virtual_addr);
+    } else {
+      throw StringError("No valid pointer size for binary");
+    };
+  };
+
   std::map<uint64_t, LIEF::Symbol> symbol_map;
   LIEF::Binary &binary;
   int pointer_size_for_binary;
-};
-
-class StringError : public std::exception {
-public:
-  StringError(std::string s) : s(s){};
-  virtual const char *what() const noexcept override { return s.c_str(); };
-
-private:
-  std::string s;
 };
